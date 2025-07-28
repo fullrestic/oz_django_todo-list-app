2c0c3bf9277be383b790e983f07c25e76707f868
fullrestic <restinplastic@naver.com>
2025-07-28 16:05:14 +0900

    ✨feat: chapter_04 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', views.todo_list, name='todo_list'),
     path('todo/<int:todo_id>/', views.todo_info, name='todo_info'),
+    path('todo/create/', views.todo_create, name='todo_create'),
+    path('todo/<int:todo_id>/update', views.todo_update, name='todo_update'),
+    path('todo/<int:todo_id>/delete', views.todo_delete, name='todo_delete'),
+    
     path('accounts/login/', user_views.login, name='login'),
     path('accounts/logout/', include('django.contrib.auth.urls'), name='logout'),
     path('signup/', user_views.sign_up, name='signup'),
